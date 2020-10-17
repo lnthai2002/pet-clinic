8adbca1eb2a9196990f80aa274980560d92729a6
Thai Le <lnthai2002@gmail.com>
2020-10-17 18:39:01 -0400

    initial commit

---
+++ src/test/java/info/noip/darkportal/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package info.noip.darkportal.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
